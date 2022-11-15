9e9e218146346d6b9663beec59da38b4f6529615
DanyalMD <117356673+DanyalMD@users.noreply.github.com>
2022-11-15 17:17:28 +0500

    Add files via upload

---
+++ SDKFramework/SDKFramework.podspec
@@ -0,0 +1,10 @@
+
+
+Pod::Spec.new do |spec|
+
+  spec.name         = "SDKFramework"
+  spec.version      = "1.0.0"
+  spec.summary      = "A short meangningful description of SDKFramework."
+  spec.description  = "The SDK specifically written for Gaming Profiles"
+  
+end
---
+++ SDKFramework/SDKFramework/SDKFramework.h
@@ -0,0 +1,18 @@
+//
+//  SDKFramework.h
+//  SDKFramework
+//
+//  Created by apple on 15/11/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDKFramework.
+FOUNDATION_EXPORT double SDKFrameworkVersionNumber;
+
+//! Project version string for SDKFramework.
+FOUNDATION_EXPORT const unsigned char SDKFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKFramework/PublicHeader.h>
+
+
